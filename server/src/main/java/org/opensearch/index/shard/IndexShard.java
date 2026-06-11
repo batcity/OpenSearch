352b468d17771c4081bce4c3ea4a8a42bb73dae2
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2026-06-11 18:54:50 +0530

    cleaning up some code

--- server/src/main/java/org/opensearch/index/shard/IndexShard.java
+++ server/src/main/java/org/opensearch/index/shard/IndexShard.java
@@ -6318,11 +6318,10 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
             return new FieldStats(0, fieldsLimit);
         }
 
-        // Filter out mappers that extend MetadataFieldMapper.
-        // This isolates user-defined fields from system metadata fields.
+        // Call the instance method on mapperService instead of a static reference
         long currentFieldsCount = java.util.stream.StreamSupport
             .stream(mapperService.documentMapper().mappers().spliterator(), false)
-            .filter(mapper -> !(mapper instanceof org.opensearch.index.mapper.MetadataFieldMapper))
+            .filter(mapper -> !mapperService.isMetadataField(mapper.name()))
             .count();
 
         return new FieldStats(currentFieldsCount, fieldsLimit);
